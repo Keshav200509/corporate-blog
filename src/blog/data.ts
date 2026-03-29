0f64c872d3b9036742c8a6ddb0a5639aec869b61
Keshav200509 <anjuyadav96689@gmail.com>
2026-03-29 23:02:05 +0530

    Remove commented-out code in data.ts

--- src/blog/data.ts
+++ src/blog/data.ts
@@ -1,42 +1,17 @@
-// import type { BlogPost, BlogPostFilters } from "./types";
-// import { getPublishedPostBySlug as getPublishedPostBySlugFromService, listPublishedPosts } from "./services/post-service";
-
-// function hasDatabase() {
-//   return Boolean(process.env.DATABASE_URL);
-// }
-
-// export async function getPublishedPosts(filters?: BlogPostFilters): Promise<BlogPost[]> {
-//   if (!hasDatabase()) {
-//     return [];
-//   }
-
-//   return listPublishedPosts(filters);
-// }
-
-// export async function getPublishedPostBySlug(slug: string): Promise<BlogPost | null> {
-//   if (!hasDatabase()) {
-//     return null;
-//   }
-
-//   return getPublishedPostBySlugFromService(slug);
-// }
-
 import type { BlogPost, BlogPostFilters } from "./types";
 import { getPublishedPostBySlug as getPublishedPostBySlugFromService, listPublishedPosts } from "./services/post-service";
 import { hasDatabase } from "../lib/db/has-database";
- 
+
 export async function getPublishedPosts(filters?: BlogPostFilters): Promise<BlogPost[]> {
   if (!hasDatabase()) {
     return [];
   }
- 
   return listPublishedPosts(filters);
 }
- 
+
 export async function getPublishedPostBySlug(slug: string): Promise<BlogPost | null> {
   if (!hasDatabase()) {
     return null;
   }
- 
   return getPublishedPostBySlugFromService(slug);
 }
