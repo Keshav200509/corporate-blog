c002965182f130d7c3803b18ad44fce699cb25f6
Keshav200509 <anjuyadav96689@gmail.com>
2026-03-31 10:33:03 +0530

    Stabilize Netlify preview build settings and simplify data entrypoint

--- src/blog/data.ts
+++ src/blog/data.ts
@@ -1,19 +1,10 @@
 import type { BlogPost, BlogPostFilters } from "./types";
 import { getPublishedPostBySlug as getPublishedPostBySlugFromService, listPublishedPosts } from "./services/post-service";
-import { hasDatabase } from "../lib/db/has-database";
-import { filterDemoPosts, findDemoPostBySlug } from "./fallback";
-
 
 export async function getPublishedPosts(filters?: BlogPostFilters): Promise<BlogPost[]> {
-  if (!hasDatabase()) {
-    return filterDemoPosts(filters);
-  }
   return listPublishedPosts(filters);
 }
 
 export async function getPublishedPostBySlug(slug: string): Promise<BlogPost | null> {
-  if (!hasDatabase()) {
-    return findDemoPostBySlug(slug);
-  }
   return getPublishedPostBySlugFromService(slug);
 }
