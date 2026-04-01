3187d0d1c9cdcbe05f11951a8e719775331a7ec2
Keshav200509 <anjuyadav96689@gmail.com>
2026-04-01 21:47:57 +0530

    feat: expand api-driven discovery workflows and content surfaces

--- src/blog/data.ts
+++ src/blog/data.ts
@@ -1,17 +1,10 @@
 import type { BlogPost, BlogPostFilters } from "./types";
 import { getPublishedPostBySlug as getPublishedPostBySlugFromService, listPublishedPosts } from "./services/post-service";
-import { hasDatabase } from "../lib/db/has-database";
 
 export async function getPublishedPosts(filters?: BlogPostFilters): Promise<BlogPost[]> {
-  if (!hasDatabase()) {
-    return [];
-  }
   return listPublishedPosts(filters);
 }
 
 export async function getPublishedPostBySlug(slug: string): Promise<BlogPost | null> {
-  if (!hasDatabase()) {
-    return null;
-  }
   return getPublishedPostBySlugFromService(slug);
 }
